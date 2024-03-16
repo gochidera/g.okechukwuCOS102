0659d84a7768b23429e0413eb9d1a994ff7facc2
gochidera <147996767+gochidera@users.noreply.github.com>
2024-03-16 19:50:01 +0100

    Delete Custom Office Templates directory

--- Custom Office Templates/practice-3/target/debug/deps/practice_3.d
+++
@@ -1,5 +0,0 @@
-C:\Users\glori\OneDrive\Documents\g.okechukwuCOS101\week-9\practice-3\target\debug\deps\practice_3.exe: src\main.rs
-
-C:\Users\glori\OneDrive\Documents\g.okechukwuCOS101\week-9\practice-3\target\debug\deps\practice_3.d: src\main.rs
-
-src\main.rs:
--- Custom Office Templates/practice-3/target/debug/practice-3.d
+++
@@ -1,1 +0,0 @@
-C:\Users\glori\OneDrive\Documents\g.okechukwuCOS101\week-9\practice-3\target\debug\practice-3.exe: C:\Users\glori\OneDrive\Documents\g.okechukwuCOS101\week-9\practice-3\src\main.rs
